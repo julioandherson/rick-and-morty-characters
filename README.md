433d6c30065f1b12be7884071e32ed9e3fa4ba55
Júlio Andherson <julio.oliveira@clickbus.com>
2021-05-04 17:55:24 -0300

    Initial project struct

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+![Xcode 12.0+](https://img.shields.io/badge/Xcode-12.0%2B-blue.svg)
+![Swift 5.0+](https://img.shields.io/badge/Swift-5.0%2B-orange.svg)
+![iOS 14.0+](https://img.shields.io/badge/iOS-14.0%2B-blue.svg)
+
 # rick-and-morty-characters
---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Julio Oliveira on 04/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
